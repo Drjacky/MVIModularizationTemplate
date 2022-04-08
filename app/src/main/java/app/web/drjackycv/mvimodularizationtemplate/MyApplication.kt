030c64d19d4d171e8947b8b9e094842b4ed28f25
Drjacky <dr.jacky.2005@gmail.com>
2022-04-08 17:06:52 +0100

    Create application class

---
+++ app/src/main/java/app/web/drjackycv/mvimodularizationtemplate/MyApplication.kt
@@ -0,0 +1,38 @@
+package app.web.drjackycv.mvimodularizationtemplate
+
+import android.os.StrictMode
+import android.os.StrictMode.ThreadPolicy
+import android.os.StrictMode.VmPolicy
+import androidx.multidex.MultiDexApplication
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+import javax.inject.Inject
+
+
+@HiltAndroidApp
+class MyApplication : MultiDexApplication() {
+
+    override fun onCreate() {
+        if (BuildConfig.DEBUG) {
+            StrictMode.setThreadPolicy(
+                ThreadPolicy.Builder()
+                    .detectDiskReads()
+                    .detectDiskWrites()
+                    .detectNetwork() // or .detectAll() for all detectable problems
+                    .penaltyLog()
+                    .build()
+            )
+            StrictMode.setVmPolicy(
+                VmPolicy.Builder()
+                    .detectLeakedSqlLiteObjects()
+                    .detectLeakedClosableObjects()
+                    .penaltyLog()
+                    //.penaltyDeath() //TODO
+                    .build()
+            )
+        }
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
+
+}
