7fe1feaef1593c2d94b66316a781ff2bfb6ae396
Drjacky <dr.jacky.2005@gmail.com>
2022-04-12 11:09:02 +0100

    Create basics of features main module

---
+++ features/main/build.gradle.kts
@@ -0,0 +1,105 @@
+import app.web.drjackycv.buildsrc.Depends
+
+plugins {
+    id("com.android.library")
+    kotlin("android")
+    id("kotlin-parcelize")
+    kotlin("kapt")
+    id("dagger.hilt.android.plugin")
+}
+
+android {
+
+    compileSdk = Depends.Versions.androidCompileSdkVersion
+
+    defaultConfig {
+        multiDexEnabled = true
+        vectorDrawables.useSupportLibrary = true
+        minSdk = Depends.Versions.minSdkVersion
+        targetSdk = Depends.Versions.targetSdkVersion
+        testInstrumentationRunner =
+            Depends.Versions.testInstrumentationRunner
+        consumerProguardFiles("consumer-rules.pro")
+    }
+    compileOptions {
+        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+
+    sourceSets {
+        val test by getting
+
+        map {
+            it.java.srcDir("src/${it.name}/kotlin")
+            //test.java.srcDir("${project(":domain").projectDir}/src/test/java")
+        }
+    }
+    buildTypes {
+        named("debug") {
+            buildConfigField(
+                "String",
+                "BASE_URL",
+                "\"" + Depends.Environments.debugBaseUrl + "\""
+            )
+        }
+        named("release") {
+            isMinifyEnabled = true
+            buildConfigField(
+                "String",
+                "BASE_URL",
+                "\"" + Depends.Environments.releaseBaseUrl + "\""
+            )
+            setProguardFiles(
+                listOf(
+                    getDefaultProguardFile("proguard-android-optimize.txt"),
+                    "proguard-rules.pro"
+                )
+            )
+        }
+    }
+
+}
+
+dependencies {
+    implementation(Depends.Libraries.kotlin)
+    implementation(Depends.Libraries.android_core_ktx)
+    implementation(Depends.Libraries.paging_runtime_ktx)
+    implementation(Depends.Libraries.paging_rx)
+    implementation(Depends.Libraries.multidex)
+    //dependency injection
+    implementation(Depends.Libraries.hilt_android)
+    kapt(Depends.Libraries.hilt_android_compiler)
+    kapt(Depends.Libraries.hilt_compiler)
+    implementation(Depends.Libraries.java_inject)
+    //network
+    implementation(Depends.Libraries.retrofit)
+    implementation(Depends.Libraries.retrofit_adapter_rx)
+    implementation(Depends.Libraries.logging_interceptor)
+    debugImplementation(Depends.Libraries.chucker)
+    releaseImplementation(Depends.Libraries.chucker_no_op)
+    implementation(Depends.Libraries.apollo_graphql_runtime)
+    implementation(Depends.Libraries.arrow)
+    //other
+    implementation(Depends.Libraries.timber)
+    implementation(Depends.Libraries.material)
+    //ui
+    implementation(Depends.Libraries.glide)
+    kapt(Depends.Libraries.glide_compiler)
+    implementation(Depends.Libraries.lottie)
+    //test
+    testImplementation(Depends.Libraries.junit)
+    testImplementation(Depends.Libraries.test_ext_junit)
+    testImplementation(Depends.Libraries.mockito_core)
+    testImplementation(Depends.Libraries.mockito_inline)
+    testImplementation(Depends.Libraries.mockito_kotlin)
+    testImplementation(Depends.Libraries.mockk)
+    testImplementation(Depends.Libraries.coroutines_test)
+    testImplementation(Depends.Libraries.arch_core_testing)
+}
