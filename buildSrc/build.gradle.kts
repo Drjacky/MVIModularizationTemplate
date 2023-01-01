4b1cee7ba9cf9899f5397f874a6fb6529f260621
Drjacky <dr.jacky.2005@gmail.com>
2023-01-01 14:15:52 +0000

    Update dependencies

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -11,8 +11,8 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin.jvm:org.jetbrains.kotlin.jvm.gradle.plugin:1.7.20")
-    implementation("com.android.tools.build:gradle:7.3.0")
-    implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.22.0-RC2")
+    implementation("org.jetbrains.kotlin.jvm:org.jetbrains.kotlin.jvm.gradle.plugin:1.7.21")
+    implementation("com.android.tools.build:gradle:7.3.1")
+    implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.22.0")
     implementation("com.squareup:javapoet:1.13.0")
 }
